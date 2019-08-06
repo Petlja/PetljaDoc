4378f01733169a7672e26a5f05cd224fd411abf9
vasiljevic <nebojsa.vasiljevic@gmail.com>
2019-08-06 13:13:25 +0200

    setup.py updated

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Petlja/PetljaDoc",
+    install_requires=dependencies,
     include_package_data=True,
     zip_safe=False,
     packages=setuptools.find_packages(),
