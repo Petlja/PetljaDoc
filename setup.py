db8b258f7bff8cd5d54d7b21316b9ace7f00f2b6
vasiljevic <nebojsa.vasiljevic@gmail.com>
2019-08-06 15:10:39 +0200

    non-PyPI dependency on runestone

--- setup.py
+++ setup.py
@@ -1,23 +1,31 @@
+import io
+import re
 import setuptools
-import petljadoc
 
-with open("README.md", "r") as fh:
+with open("README.md", "rt", encoding='utf8') as fh:
     long_description = fh.read()
 
-with open('requirements.txt', 'r') as fh:
-    dependencies = [l.strip() for l in fh if not l.strip()]
+install_requires= []
+with open('requirements.txt', 'rt', encoding='utf8') as fh:
+    for l in fh:
+        install_requires.append(l.strip())
+
+with io.open('petljadoc/__init__.py', 'rt', encoding='utf8') as f:
+    src = f.read()
+m = re.search(r'\_\_version\_\_\s*=\s*\"([^"]*)\"', src)
+version = m.group(1)
 
 setuptools.setup(
     python_requires=">=3.6",
     name="petljadoc",
-    version=petljadoc.__version__,
+    version=version,
     author="Fondacija Petlja",
     author_email="team@petlja.org",
     description="Petlja's command-line interface for learning content",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Petlja/PetljaDoc",
-    install_requires=dependencies,
+    install_requires=install_requires,
     include_package_data=True,
     zip_safe=False,
     packages=setuptools.find_packages(),
