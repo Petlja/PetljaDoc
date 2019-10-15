f9fc9dce5ea931ac9bb7f7b82699a3ff2351a30e
NenadMilutinovic <nenad.milutinovic@petlja.org>
2019-10-15 12:22:11 +0200

    Subfolders fix

--- petljadoc/cyr2lat.py
+++ petljadoc/cyr2lat.py
@@ -15,16 +15,20 @@ def cyr2latTranslate(src_dir, dest_dir):
     for item in os.listdir(src_dir):
         extension = os.path.splitext(item)[1][1:]
         s = os.path.join(src_dir, item) 
-        d = os.path.join(dest_dir, item)
-        shutil.copyfile(s,d)
-        f = open(s, encoding="utf8")
-        content = f.read()
-        newF = open(d, "w", encoding="utf8")
-        newF.truncate(0)
-        
-        if extension in extensionList:
-            newF.write(cyrtranslit.to_latin(content, 'sr'))
+        if os.path.isdir(s):
+            d = os.path.join(dest_dir, item)
+            cyr2latTranslate(s, d)
         else:
-            newF.write(content)
-        newF.close()
-        #print(f"C {s} -> {d}")
+            d = os.path.join(dest_dir, item)
+            shutil.copyfile(s,d)
+            f = open(s, encoding="utf8")
+            content = f.read()
+            newF = open(d, "w", encoding="utf8")
+            newF.truncate(0)
+            
+            if extension in extensionList:
+                newF.write(cyrtranslit.to_latin(content, 'sr'))
+            else:
+                newF.write(content)
+            newF.close()
+            #print(f"C {s} -> {d}")
