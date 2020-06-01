16f564f39ed2e9d1c60308fe0cbca8223bd3bafc
Jovan Maric <33927662+maric993@users.noreply.github.com>
2020-06-01 21:52:57 +0200

    Error handling

--- petljadoc/cli.py
+++ petljadoc/cli.py
@@ -7,11 +7,13 @@ from pathlib import Path
 import getpass
 import click
 import yaml
+from colorama import Fore,init,Style
 from pkg_resources import resource_filename
 from paver.easy import sh
 from petljadoc import bootstrap_petlja_theme
 from .templateutil import apply_template_dir, default_template_arguments
 from .cyr2lat import cyr2latTranslate
+from .course import Activity,Lesson,Course,PetljadocError
 
 INDEX_TEMPLATE_HIDDEN = '''
 .. toctree:: 
@@ -65,7 +67,7 @@ def init_template_arguments(template_dir, defaults,project_type):
     ta['downloads_enabled'] = 'false'
     ta['templates_path'] = '_templates'
     ta['html_theme_path'] = '_templates/plugin_layouts'
-    custom_theme = _prompt("Copy HTML theme into project",type=bool, 
+    custom_theme = _prompt("Copy HTML theme into project",type=bool,
                            default="yes", force_default=defaults)
     if custom_theme:
         ta['html_theme'] = 'custom_theme'
@@ -99,48 +101,37 @@ def _prompt(text, default=None, hide_input=False, confirmation_prompt=False,
 def parse_yaml(path):
     with open('_sources/index.yaml', encoding='utf8') as f:
         data = yaml.load(f, Loader=yaml.FullLoader,)
+        course = check_structure(data)
         index = open('_intermediate/index.rst',mode = 'w+',encoding='utf-8')
-        index.write('**Ime Kursa**: *'+data['title']+'*\n')
-        index.write('\n')
-        index.write('Sta cete nauciti:\n')
-        for i in data['description']['willLearn']:
-            index.write(' '*4+'- '+i+'\n')
-        index.write('\n')
-        index.write('Potrebon:\n')
-        for i in data['description']['requirements']:
-            index.write(' '*4+'- '+i+'\n')
-        index.write('\n')
-        index.write('Sadrzaj kursa:'+'\n')
-        for i in data['description']['toc']:
-            index.write(' '*4+'- '+i+'\n')
-        index.write('\n')
-        index.write(INDEX_TEMPLATE_HIDDEN.format(3))
+        write_to_index(index,course)
         path = path.joinpath('_intermediate')
-        for i in data['lessons'][1:]:
-            copy_dir('_sources/'+i['lesson']['title'],'_intermediate/'+i['lesson']['title'])
-            index.write(' '*4+ i['lesson']['title']+'/index\n')
-            section_index = open(path.joinpath(i['lesson']['title']).joinpath('index.rst'),
+        for lesson in course.active_lessons:
+            copy_dir('_sources/'+lesson.title,'_intermediate/'+lesson.title)
+            index.write(' '*4+ lesson.title +'/index\n')
+            section_index = open(path.joinpath(lesson.title).joinpath('index.rst'),
                                  mode = 'w+',
                                  encoding='utf-8')
-            section_index.write("="*len(i['lesson']['title'])+'\n'+i['lesson']['title']+'\n'+"="*len(i['lesson']['title'])+'\n')
+            section_index.write("="*len(lesson.title)+'\n'+
+                                lesson.title+'\n'+
+                                "="*len(lesson.title)+'\n')
             section_index.write(INDEX_TEMPLATE.format(1))
-            for a in i['lesson']['activities'][1:]:
-                if "url" not in a:
-                    if a['file'].rsplit('.')[1] == 'rst':
-                        section_index.write(' '*4+a['file']+'\n')
-                    if a['file'].rsplit('.')[1] == 'pdf':
-                        pdf_rst = open('_intermediate/'+i['lesson']['title']+'/'+a['title']+'.rst',
+            for activity in lesson.active_activies:
+                if activity.activity_type in ['reading','quiz']:
+                    if activity.get_src_ext() == 'rst':
+                        section_index.write(' '*4+activity.src+'\n')
+                    if activity.get_src_ext() == 'pdf':
+                        pdf_rst = open('_intermediate/'+lesson.title+'/'+activity.title+'.rst',
+                                       mode = 'w+',encoding='utf-8')
+                        pdf_rst.write(activity.title+'\n'+"="*len(activity.title)+'\n')
+                        pdf_rst.write(PDF_TEMPLATE.format('/_static/'+activity.src))
+                        section_index.write(' '*4+activity.title+'.rst\n')
+                if activity.activity_type == 'video':
+                    video_rst = open('_intermediate/'+lesson.title+'/'+activity.title+'.rst',
                                      mode = 'w+',encoding='utf-8')
-                        pdf_rst.write(a['title']+'\n'+"="*len(a['title'])+'\n')
-                        pdf_rst.write(PDF_TEMPLATE.format('/_static/'+a['file']))
-                        section_index.write(' '*4+a['title']+'.rst\n')
-                else:
-                    video_rst = open('_intermediate/'+i['lesson']['title']+'/'+a['title']+'.rst',
-                                     mode = 'w+',encoding='utf-8')
-                    video_rst.write(a['title']+'\n'+"="*len(a['title'])+'\n')
-                    video_rst.write(YOUTUBE_TEMPLATE.format(a['url'].rsplit('/',1)[1]))
-                    section_index.write(' '*4+a['title']+'.rst\n')
-           
+                    video_rst.write(activity.title+'\n'+"="*len(activity.title)+'\n')
+                    video_rst.write(YOUTUBE_TEMPLATE.format(activity.src.rsplit('/',1)[1]))
+                    section_index.write(' '*4+activity.title+'.rst\n')
+
 def prebuild():
     p = Path(os.getcwd())
     if not p.joinpath('_sources/index.yaml').exists():
@@ -300,3 +291,114 @@ def cyr2lat():
         cyr2latTranslate(sourcePath, destinationPath)
     else:
         print('Folder name must end with Cyrl')
+
+def check_structure(data):
+    error_log = {}
+    archived_lessons = []
+    active_lessons = []
+    error_log['courseId'], courseId = check_component(data,'courseId',PetljadocError.ERROR_ID)
+    error_log['lang'], lang = check_component(data, 'lang', PetljadocError.ERROR_LANG)
+    error_log['title'], title_course = check_component(data, 'title', PetljadocError.ERROR_TITLE)
+    error_log['description'],_ = check_component(data, 'description', PetljadocError.ERROR_DESC)
+    if error_log['description']:
+        error_log['willLearn'] ,willLearn = check_component(data['description'],'willLearn',PetljadocError.ERROR_WILL_LEARN)
+        error_log['requirements'] ,requirements = check_component(data['description'],'requirements',PetljadocError.ERROR_REQUIREMENTS)
+        error_log['toc'], toc = check_component(data['description'],'toc',PetljadocError.ERROR_TOC)
+        error_log['externalLinks'], externalLinks = check_component(data['description'],'externalLinks','',False)
+    error_log['lessons'], _ = check_component(data, 'lessons', PetljadocError.ERROR_LESSONS)
+    if error_log['lessons']:
+        lessons = data['lessons'][0]
+        start_index = 0
+        if 'archived' in lessons:
+            start_index = 1
+            for arhived_lesson in lessons['archived']:
+                error_log['archived_lesson_guid'], archived_guid =  check_component(arhived_lesson,'guid','')
+                if error_log['archived_lesson_guid']:
+                    archived_lessons.append(archived_guid)
+        for i,lesson in enumerate(data['lessons'][start_index:],start=1):
+            error_log[str(i)+'_lesson_title'], title = check_component(lesson['lesson'],'title',PetljadocError.ERROR_LESSON_TITLE.format(i))
+            error_log[str(i)+'_lesson_guid'],guid = check_component(lesson['lesson'],'guid',PetljadocError.ERROR_LESSON_GUID.format(i))
+            error_log[str(i)+'_lesson_description'], description = check_component(lesson['lesson'],'description','',False)
+            error_log[str(i)+'_lesson_activities'], lesson_activities = check_component(lesson['lesson'],'activities',PetljadocError.ERROR_LESSON_ACTIVITIES.format(i))
+            if error_log[str(i)+'_lesson_activities']:
+                first_lesson_activities = lesson_activities[0]
+                start_index_activities = 0
+                archived_activities = []
+                if 'archived' in first_lesson_activities:
+                    start_index_activities = 1
+                    if first_lesson_activities['archived']:
+                        for archived_activity in first_lesson_activities['archived']:
+                            error_log[str(i)+'_archived_activity_guid'], archived_activity_guid =  check_component(archived_activity,'guid','')
+                            if error_log[str(i)+'_archived_activity_guid']:
+                                archived_activities.append(archived_activity_guid)
+                active_activies =[]
+                for j,activity in enumerate(lesson_activities[start_index_activities:],start=1):
+                    activity_type =list(activity.keys())[0]
+                    error_log[str(i)+'_'+str(j)+'_activity_title'] = activity_type in ['reading','video','quiz']
+                    if not error_log[str(i)+'_'+str(j)+'_activity_title']:
+                        print_error(PetljadocError.ERROR_UNKNOWN_ACTIVITY.format(i))
+                    error_log[str(i)+'_'+str(j)+'_activity_title'], activity_title = check_component(activity,'title',PetljadocError.ERROR_ACTIVITY_TITLE.format(i,j))
+                    error_log[str(i)+'_'+str(j)+'_activity_guid'], activity_guid = check_component(activity,'guid',PetljadocError.ERROR_ACTIVITY_GUID.format(i,j))
+                    error_log[str(i)+'_'+str(j)+'_activity_descripiton'], activity_description = check_component(activity,'description','',False)
+                    error_log[str(i)+'_'+str(j)+'_activity_src'], activity_src =  check_component(activity,'file','')
+                    if not error_log[str(i)+'_'+str(j)+'_activity_src']:
+                        error_log[str(i)+'_'+str(j)+'_activity_src'], activity_src =  check_component(activity,'url','')
+                    if not error_log[str(i)+'_'+str(j)+'_activity_src']:
+                        print_error(PetljadocError.ERROR_ACTIVITY_SRC)
+                    active_activies.append(Activity(activity_type,activity_title,activity_src,activity_guid,activity_description))
+                active_lessons.append(Lesson(title,guid,description,archived_activities,active_activies))
+
+    course = Course(courseId,lang,title_course,willLearn,requirements,toc,externalLinks,archived_lessons,active_lessons)
+    error_log['guid_integrity'] = course.guid_check()
+    if not course.guid_check():
+        print_error(PetljadocError.ERROR_DUPLICATE_GUID)
+    error_log['source_integrity'],missing_src = course.source_check()
+    if not error_log['source_integrity']:
+        print_error(PetljadocError.ERROR_SOURCE_MISSING)
+        for src in missing_src:
+            print(src)
+        print()
+    if False in error_log.values():
+        print_error(PetljadocError.ERROR_MSG_BUILD)
+        exit(-1)
+
+    return course
+
+def check_component(dictionary,component,error_msg,required = True):
+    try:
+        item = dictionary[component]
+    except KeyError:
+        if required:
+            if error_msg != '':
+                print_error(error_msg)
+            return [False,'']
+        else:
+            return [True,'']
+    else:
+        return [True,item]
+
+
+def write_to_index(index,course):
+    index.write("="*len(course.title)+'\n'+
+                course.title+'\n'+
+                "="*len(course.title)+'\n')
+    index.write('\n')
+    index.write('Sta cete nauciti:\n')
+    for willlearn in course.willlearn:
+        index.write(' '*4+'- '+willlearn+'\n')
+    index.write('\n')
+    index.write('Potrebon:\n')
+    for requirements in course.requirements:
+        index.write(' '*4+'- '+requirements+'\n')
+    index.write('\n')
+    index.write('Sadrzaj kursa:'+'\n')
+    for toc in course.toc:
+        index.write(' '*4+'- '+toc+'\n')
+    index.write('\n')
+    index.write(INDEX_TEMPLATE_HIDDEN.format(3))
+
+
+def print_error(error):
+    init()
+    print(Fore.RED, error)
+    print(Style.RESET_ALL)
