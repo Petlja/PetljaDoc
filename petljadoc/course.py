c4d4ec49e5a837010ffa7e4012c5ec22892b9e14
vugdelija <milanvugdelija@yahoo.com>
2020-07-02 17:29:01 +0200

    typo

--- petljadoc/course.py
+++ petljadoc/course.py
@@ -120,7 +120,7 @@ class PetljadocError:
     ERROR_YAML_TYPE_ERROR = 'Yaml stucture error.'
 
 class LanguagePick:
-    CourseDescripiton = {'sr-Cyrl':{'willLearn':'Шта ће те научити:\n','requirements':'Потребно:\n','toc':'Садржај:\n','externalLinks':'Додатни линкови:\n'},
+    CourseDescripiton = {'sr-Cyrl':{'willLearn':'Шта ћете научити:\n','requirements':'Потребно:\n','toc':'Садржај:\n','externalLinks':'Додатни линкови:\n'},
                          'sr-Latn':{'willLearn':'Sta ćete naučiti:\n','requirements':'Potrebno:\n','toc':'Sadržaj:\n','externalLinks':'Dodatni linkovi:\n'},
                          'en':{'willLearn':'Things you will learn:\n','requirements':'Required:\n','toc':'Table of content:\n','externalLinks':'External links:\n'}}
     def __init__(self,lang):
