0d9d1c335b8a0bc1db240ba051cfea1cf7f08639
Jovan Maric <33927662+maric993@users.noreply.github.com>
2022-04-26 15:17:55 +0200

    WIP

--- petljadoc/runestone_ext/regexcheck/js/regex-check.js
+++ petljadoc/runestone_ext/regexcheck/js/regex-check.js
@@ -4,18 +4,38 @@ window.addEventListener("load",function(){
     customArea = document.getElementById("front");
     text_area.addEventListener("input", function(){
         text_area = document.getElementById("text_input");
-        customArea.innerHTML = text_area.value.replaceAll('\n' ,'<br>');
+        customArea.innerHTML = text_area.value.replaceAll('\n' ,'<br>').replaceAll('\t', '&emsp;').replaceAll(" </span>", "&nbsp;</span>");
     });
     regex_area.addEventListener("input", function(){
          if (regex_area.value == ""){
-            customArea.innerHTML = text_area.value.replaceAll('\n' ,'<br>');
+            customArea.innerHTML = text_area.value.replaceAll('\n' ,'<br>').replaceAll('\t', '&emsp;').replaceAll(" </span>", "&nbsp;</span>");
             return 
          }
-         customArea.innerHTML = text_area.value.replaceAll('\n' ,'<br>');
+         var text = text_area.value
          var re = new RegExp("("+ regex_area.value+ ")",flag="g");
          text_area = document.getElementById("text_input");
-         customArea.innerHTML = customArea.innerHTML.replace(re,"<span class='blue'>$1</span>");
+         customArea.innerHTML = text.replace(re,"<span class='blue'>$1</span>").replaceAll('\n' ,'<br>').replaceAll('\t', '&emsp;').replaceAll(" </span>", "&nbsp;</span>");
      });
+
+     text_area.addEventListener('keydown', function(e) {
+        if (e.key == 'Tab') {
+          e.preventDefault();
+          var start = this.selectionStart;
+          var end = this.selectionEnd;
+      
+          // set textarea value to: text before caret + tab + text after caret
+          this.value = this.value.substring(0, start) +
+            "\t" + this.value.substring(end);
+      
+          // put caret at right position again
+          this.selectionStart =
+            this.selectionEnd = start + 1;
+
+        text_area = document.getElementById("text_input");
+        customArea.innerHTML = text_area.value.replaceAll('\n' ,'<br>').replaceAll('\t', '&emsp;').replaceAll(" </span>", "&nbsp;</span>");
+        }
+
+      });
  });
 
 
