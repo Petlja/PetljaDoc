b8b2c0ce053fb5c1e605bcd5f33ada1381cf44eb
Nemanja Vuković <nemanja16vukovic@gmail.com>
2025-11-20 14:17:29 +0100

    Revert "notes- sređeno"
    This reverts commit 11b27ea78aa125a4e6ae53e6da7000b24b74d83f.

--- petljadoc/runestone_ext/notes/js/notes.js
+++ petljadoc/runestone_ext/notes/js/notes.js
@@ -8,53 +8,11 @@ $(document).ready(function () {
     if (document.querySelector('.learnmorenote-type .course-content ')) {
         var allNodes = document.querySelectorAll('.learnmorenote-type .course-content ');
         for (var i = 0; i < allNodes.length; i++) {
-            
             hideContent(allNodes[i])
-
-            var arrow = document.createElement("span");
-            arrow.className = "toggle-arrow closed";
-            arrow.setAttribute("role", "button");
-            arrow.textContent = "﹀";
-            arrow.setAttribute("aria-expanded", "false");
-            arrow.setAttribute("tabindex", "0")
-
-            allNodes[i].children[0].appendChild(arrow);
-            allNodes[i].children[0].className = "note-title";
-            allNodes[i].children[0].setAttribute(
-                                "aria-label",
-                                "Ова напомена садржи додатне информације. Кликните да прикажете скривени садржај.")
-            arrow.addEventListener("click", function (ev) {
-                ev.stopPropagation();
-                this.parentNode.click();
-            });
-            
             allNodes[i].children[0].addEventListener("click", function () {
                 this.classList.toggle('expanded');
                 toggleVisible(this.parentNode);
-
-                var arrow = this.querySelector(".toggle-arrow");
-                var content = this.parentNode;
-
-                if (this.classList.contains("expanded")) {
-                    arrow.classList.remove("closed");
-                    arrow.classList.add("opened");
-                    arrow.textContent = "︿";
-                    arrow.setAttribute("aria-expanded", "true");
-                } else {
-                    arrow.classList.remove("opened");
-                    arrow.classList.add("closed");
-                    arrow.textContent = "﹀";
-
-                    arrow.setAttribute("aria-expanded", "false");
-                }
             })
-
-            allNodes[i].children[0].addEventListener("keydown", function (e) {
-                if (e.key === "Enter" || e.key === " ") {
-                    e.preventDefault();
-                    this.click();
-                }
-            });
         }
     }
 });
@@ -72,10 +30,6 @@ function hideContent(learnDiv) {
 
     if (contentParagraph.length == 0) {
         var titleParaghraph = document.createElement("p");
-        titleParaghraph.className = "note-title";
-        titleParaghraph.setAttribute(
-        "aria-label",
-        "Ова напомена садржи додатне информације. Кликните да прикажете скривени садржај.")
         learnDiv.prepend(titleParaghraph);
     }
 
